68d356ece8e34836bdb8b9bc942244bd9d970787
EugeneStranger <Stranger_1254@mail.ru>
2023-09-22 12:40:55 +0300

    3 Class Done

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
-
+#define delimiter "\n--------------------------------------------------------------------\n"
 class Human
 {
 protected:
@@ -116,17 +116,99 @@ public:
 
 class Teacher :public Human
 {
-
+	std::string speciality;
+	int experience;
+public:
+	const std::string get_speciality()
+	{
+		return speciality;
+	}
+	int get_experience()const
+	{
+		return experience;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_experience(int experience)
+	{
+		this->experience = experience;
+	}
+	Teacher 
+	(
+		const std::string& last_name, const std::string& first_name, int age,
+		const std::string& speciality, int experience
+		):Human (last_name, first_name, age)
+	{
+		set_speciality(speciality);
+		set_experience(experience);
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Teacher()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+	void print()const
+	{
+		Human::print();
+		cout << speciality << " " << experience << endl;
+	}
 };
 
+class Graduate : public Student
+{
+	std::string subject;
+public:
+	const std::string get_subject()
+	{
+		return subject;
+	}
+	void set_subject(const std::string& subject)
+	{
+		this->subject = subject;
+	}
+	Graduate
+	(
+		const std::string& last_name, const std::string& first_name, int age,
+		const std::string& speciality, const std::string& group, double rating, double attendence,
+		const std::string& subject
+	) :Student(last_name, first_name, age, speciality, group, rating, attendence)
+	{
+		set_subject(subject);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+	void print()const
+	{
+		Student::print();
+		cout << subject << endl;
+	}
+};
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	Human human("Montana", "Antonio", 30);
+	cout << delimiter;
 	human.print();
+	cout << delimiter;
 
 	Student stud("Pinkman","Jessie", 25, "Chemistry","WW_220", 95, 98);
+	cout << delimiter;
 	stud.print();
+	cout << delimiter;
 
+	Teacher teacher("White", "Walter", 50, "Chemistry", 20);
+	cout << delimiter;
+	teacher.print();
+	cout << delimiter;
+
+	Graduate grad("Schrader", "Hank", 40, "Criminalistic", "OBN", 50, 50, "How to catch");
+	cout << delimiter;
+	grad.print();
+	cout << delimiter;
 }
